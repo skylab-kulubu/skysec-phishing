2f78696feb4876522e26db797573e9d804d84e44
farukerdem <farukomererdem2003@gmail.com>
2024-10-26 00:58:28 +0300

    Compose PostgreSQL

--- phishing/phishing/settings.py
+++ phishing/phishing/settings.py
@@ -77,11 +77,23 @@ WSGI_APPLICATION = 'phishing.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "phisher",
+        "USER": "phisher",
+        "PASSWORD": "postgres_password",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
