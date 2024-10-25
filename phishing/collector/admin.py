756b61da5356daf92d072a942df0303d0a91a93c
farukerdem <farukomererdem2003@gmail.com>
2024-10-26 01:48:37 +0300

    Admin page improvement

--- phishing/collector/admin.py
+++ phishing/collector/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Target
+from .models import Target,User
 
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ["first_name","last_name","count","is_completed"]
+    list_filter = ["is_completed","count"]
+    def first_name(slef,Target):
+        return Target.user.first_name
+    
+    def last_name(slef,Target):
+        return Target.user.last_name
 
-
-admin.site.register(Target)
+admin.site.register(Target,TargetAdmin)
--- phishing/quizapp/admin.py
+++ phishing/quizapp/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Quiz
 # Register your models here.
 
-admin.site.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ["question", "is_active"]
+    list_filter = ["is_active"]
+
+admin.site.register(Quiz,QuizAdmin)
